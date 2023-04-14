0b4604f693d82de19c50985fae1b2089c9ecc262
MatiasHaspert <mati2002haspert@gmail.com>
2023-04-14 17:17:23 -0300

    cambio siemple

--- MACROS/Ejercicio1-1.c
+++ MACROS/Ejercicio1-1.c
@@ -7,7 +7,7 @@ int main()
 {
     int num1;
     int num2;
-    int a;
+    
     printf("Te dire el mínimo entre dos numeros\n");
     printf("Ingrese un numero:\n");
     scanf("%d", &num1);
@@ -21,9 +21,7 @@ int main()
         return 0;
     }
 
-    a = MIN(num1,num2);
-
-    printf("El minimo entre %d y %d es: %d\n", num1,num2,a);
+    printf("El minimo entre %d y %d es: %d\n", num1,num2,MIN(num1,num2));
 
     return 0; 
 
